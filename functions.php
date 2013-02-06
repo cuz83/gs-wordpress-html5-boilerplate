c6d9abcfc8f4a80061fc72763e44d3c6ef302e30
Simon MacFadyen <simon.macfadyen@greensplash.com>
2013-02-06 11:59:42 +0000

    Added local tweet to boilerplate

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 require_once locate_template('/includes/init.php'); 				      // Theme setup, initial creations
 require_once locate_template('/includes/scripts.php');				    // CSS and JS
 require_once locate_template('/includes/custom-functions.php');		// Custom functions
+require_once local_template('/includes/local-tweet.php');         // Include a local twitter API, credit https://gist.github.com/stanni
 ?>
